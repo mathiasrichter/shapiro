d617c062e6d05283a2f0b6b3b4df6d81c20084f7
guscht <uschtringuntram@hotmail.com>
2022-11-24 23:21:20 +0100

    removed js code which seems to cause an error and no functionality

--- static/scripts.js
+++ static/scripts.js
@@ -19,15 +19,6 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 200
-        });
-    };
-
 });
 
 var resetTable = function() {
