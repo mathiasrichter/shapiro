0ec348fa7a5edd2332824b7217e16940aaf79f6a
Mathias Richter <mathias.richter@gmail.com>
2023-02-14 10:33:28 +0100

    Added details for rendering errors.

--- shapiro_model.py
+++ shapiro_model.py
@@ -55,9 +55,9 @@ class SemanticModelElement(Subscriptable):
                         self.graph = Graph().parse("http://schema.org", format='json-ld')
                     else:  
                         self.graph = Graph().parse(iri)
-                except:
+                except Exception as x:
                     # happens when the overall ontology is exists, but any term contained does not resolve (e.g. if the IRIs are wrong)
-                    raise NotFoundException("Cannot resolve the requested term in the existing ontology {}".format(iri))
+                    raise NotFoundException("Cannot resolve the requested term in the existing ontology {}\nDetails: {}".format(iri,x))
             self.label, self.title, self.comment, self.description, self.definition = self.get_label_and_descriptions()
             if self.label == "" and self.title == "":
                 self.label = self.title = prune_iri(self.iri, True)
